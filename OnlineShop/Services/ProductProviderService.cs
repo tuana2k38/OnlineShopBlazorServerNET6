c9af425a540a07010ccdeb8067a0256d65cf8ae0
tuannh <tuana2k38@gmail.com>
2022-09-22 00:32:51 +0700

    inital commit

---
+++ OnlineShop/Services/ProductProviderService.cs
@@ -0,0 +1,33 @@
+﻿using OnlineShop.Models;
+using System.Collections.Immutable;
+
+namespace OnlineShop.Services
+{
+    public static class ProductProviderService
+    {
+        public static readonly ImmutableList<Product> Products = ImmutableList.CreateRange(new List<Product>()
+        {
+            new()
+            {
+                Id = Guid.NewGuid(),
+                Name = "All roads lead to Blazor Server",
+                Description = "A tutorial book for Blazor Server technology",
+                Price = 70
+            },
+            new()
+            {
+                Id = Guid.NewGuid(),
+                Name = "All roads lead to Blazor WASM",
+                Description = "A tutorial book for Blazor WASM technology.",
+                Price = 70
+            },
+            new()
+            {
+                Id = Guid.NewGuid(),
+                Name = "gRPC for Blazor WASM",
+                Description = "A tutorial book for developing a backend for Blazor WASM technology.",
+                Price = 100
+            }
+        });
+    }
+}
